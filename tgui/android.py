101a83fe870bb8e3795d27f4240812eee6f9ecab
U0_105 <hosseinabbasi80.ha@gmail.com>
2025-05-14 21:04:56 +0330

    update .gitignore to exclude some folders

--- tgui/android.py
+++
@@ -1,6 +0,0 @@
-from ppadb.client import Client as AdbClient
-def android_file(addres):
-    clint = AdbClient(host='127.0.0.1',port=5037)
-    devices = clint.devices()
-    device = devices[0]
-    return device.shell(f"cd {addres} && ls").strip().split()  
